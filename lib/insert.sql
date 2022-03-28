56687e1590975f779e6368687f85824eea0fde91
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-03-28 15:51:31 -0700

    commit tree 1fcbcebe284b1fd24636d978b6b0a61e2eb4d5c7

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+("Mr. Chocolate", 20, "M", "Brown", "calm", 1),
+("Rowdy", 10, "M", "Black", "intense", 1),
+("Tabitha", 6, "F", "Brown", "Nice", 1),
+("Sargeant Brown", 19, "M", "White", "Slimy", 1),
+("Melissa", 14, "F", "Brown", "goofy", 1),
+("Grinch", 2, "M", "Black", "Grinchy", 1),
+("Wendy", 6, "F", "White", "naive", 1),
+( NULL, 22, "M", "Black", "little mean", 1);
